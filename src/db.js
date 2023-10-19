cffb3d2a119157429bccecb26a8ef419e97aeeb8
HazzelOltra <d20390034@huauchinango.tenm.mx>
2023-10-19 17:39:05 -0600

    Modificamos Varibles

--- src/db.js
+++ src/db.js
@@ -2,10 +2,18 @@ import { createPool } from "mysql2/promise";
 
 //Realizar conexion a la BD
 
+import {
+    DB_HOST,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER,
+    DB_DATABASE
+}   from "./config.js"
+
 export const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "JaniceSalinas19",
-    port: 3306,
-    database: "triotech"
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE
 })
